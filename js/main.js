3025fec783b2d4d7c70b7b910bc75bf6e034fe13
nbyidiandian <nbyidiandian@gmail.com>
2013-10-12 19:37:55 +0800

    add memcached curr_items

--- js/main.js
+++ js/main.js
@@ -41,53 +41,63 @@ $(function () {
         // Load images via flickr for demonstration purposes:
         var gallery = $('#gallery');
 
-        var sap_link = '/data?timezone=Asia%2FShanghai&timestop=now&timestart=-10min&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&brief=1&width=750&height=200&multigraph=metric&view=html';
-        var sap_thumbnail_url = '/data?timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&multigraph=metric&width=150&height=75&view=png&brief=1';
-        var sap_fullscreen_url = '/data?timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=png&brief=1';
-        var sap_detail_link = '/data?timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=html&legend=node&sort=avg';
-        var simond_thumbnail_url = '/data?timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=simond&report=systemstate&reportItem=pernode&legend=metric&width=150&height=75&view=png&brief=1';
-        var simond_fullscreen_url = '/data?timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=simond&report=systemstate&reportItem=pernode&legend=metric&width=750&height=200&view=png&brief=1';
-        var simond_detail_link = '/data?timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=simond&report=systemstate&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=html&legend=node&sort=avg';
+        var sap_link = 'timezone=Asia%2FShanghai&timestop=now&timestart=-10min&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&brief=1&width=750&height=200&multigraph=metric&view=html';
+        var sap_thumbnail_url = 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&multigraph=metric&width=120&height=70&view=png&brief=1';
+        var sap_fullscreen_url = 'timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=png&brief=1';
+        var sap_detail_link = 'timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=%5CQsap_server_metrics_0.11%5CE&report=state&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=html&legend=node&sort=avg';
+        var simond_thumbnail_url = 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=simond&report=systemstate&reportItem=pernode&legend=metric&width=120&height=70&view=png&brief=1';
+        var simond_fullscreen_url = 'timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=simond&report=systemstate&reportItem=pernode&legend=metric&width=750&height=200&view=png&brief=1';
+        var simond_detail_link = 'timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=simond&report=systemstate&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=html&legend=node&sort=avg';
+        var cache_thumbnail_url = 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=memcached_server&report=memcached_state&reportItem=pernode&legend=metric&multigraph=metric&width=120&height=70&view=png&brief=1';
+        var cache_fullscreen_url = 'timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=memcached_server&report=memcached_state&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=png&brief=1';
+        var cache_detail_link = 'timezone=Asia%2FShanghai&timestart=-2h&timestop=now&application=memcached_server&report=memcached_state&reportItem=pernode&legend=metric&multigraph=metric&width=750&height=200&view=html&legend=node&sort=avg';
         var metrics = [
             {
                 name: 'avarage_latency',
                 thumbnail: sap_thumbnail_url + '&metric=avarage_latency',
                 fullscreen: sap_fullscreen_url + '&metric=avarage_latency',
                 details: sap_detail_link + '&metric=avarage_latency',
-                related: '/data?timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=sp_worker_metrics&report=sp_state&reportItem=pernodeservice&legend=service&legend=metric&brief=1&metric=query_resp_latency&multigraph=key.service&nonzero=1&width=750&height=200&view=html'
+                related: 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=sp_worker_metrics&report=sp_state&reportItem=pernodeservice&legend=service&legend=metric&brief=1&metric=query_resp_latency&multigraph=key.service&nonzero=1&width=750&height=200&view=html'
             },
             {
                 name: 'in_qps',
                 thumbnail: sap_thumbnail_url + '&metric=in_qps',
                 fullscreen: sap_fullscreen_url + '&metric=in_qps',
                 details: sap_detail_link + '&metric=in_qps',
-                related: '/data?timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=sp_worker_metrics&report=sp_state&reportItem=pernodesrc&legend=src&brief=1&multigraph=key.src&nonzero=1&graph=STACK&width=750&height=200&view=html'
+                related: 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=sp_worker_metrics&report=sp_state&reportItem=pernodesrc&legend=src&brief=1&multigraph=key.src&nonzero=1&graph=STACK&width=750&height=200&view=html'
             },
             {
                 name: 'searcher_res',
                 thumbnail: sap_thumbnail_url + '&metric=searcher_res',
                 fullscreen: sap_fullscreen_url + '&metric=searcher_res',
                 details: sap_detail_link + '&metric=searcher_res',
-                related: '/data?timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=simond&report=systemstate&reportItem=pernode&metric=mem_.*&legend=cluster&legend=metric&legend=node&multigraph=metric&nonzero=1&width=750&height=200&view=html'
+                related: 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=simond&report=systemstate&reportItem=pernode&metric=mem_.*&legend=cluster&legend=metric&legend=node&multigraph=metric&nonzero=1&width=750&height=200&view=html'
             },
             {
                 name: 'out_err_qps',
                 thumbnail: sap_thumbnail_url + '&metric=out_err_qps',
                 fullscreen: sap_fullscreen_url + '&metric=out_err_qps',
                 details: sap_detail_link + '&metric=out_err_qps',
-                related: '/data?timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=sp_worker_metrics&report=sp_state&reportItem=pernodeapp&legend=app&brief=5&metric=app_err_qps&multigraph=key.app&nonzero=1&width=750&height=200&view=html'
+                related: 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=sp_worker_metrics&report=sp_state&reportItem=pernodeapp&legend=app&brief=5&metric=app_err_qps&multigraph=key.app&nonzero=1&width=750&height=200&view=html'
             },
             {
                 name: 'cpu_busy',
                 thumbnail: simond_thumbnail_url + '&metric=cpu_busy%25',
                 fullscreen: simond_fullscreen_url + '&metric=cpu_busy%25',
                 details: simond_detail_link + '&metric=cpu_busy%25',
-                related: '/data?timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=simond&report=systemstate&reportItem=pernode&metric=cpu_.*&legend=cluster&legend=metric&legend=node&multigraph=metric&nonzero=1&width=750&height=200&view=html'
+                related: 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=simond&report=systemstate&reportItem=pernode&metric=cpu_.*&legend=cluster&legend=metric&legend=node&multigraph=metric&nonzero=1&width=750&height=200&view=html'
+            },
+            {
+                name: 'cached_items',
+                thumbnail: cache_thumbnail_url + '&metric=curr_items&cluster=sp',
+                fullscreen: cache_fullscreen_url + '&metric=curr_items',
+                details: cache_detail_link + '&metric=curr_items',
+                related: 'timezone=Asia%2FShanghai&timestart=-10min&timestop=now&application=memcached_server&report=memcached_state&reportItem=pernode&legend=metric&legend=node&multigraph=metric&nonzero=1&width=750&height=200&view=html'
             }
-        ];
+         ];
         var clusters = [
             {
-                cluster: 'sp_s.*',
+                cluster: 'sp|sp_.*',
                 name: 'SP',
                 server: '110.75.28.193:8000'
             }, 
@@ -103,16 +113,16 @@ $(function () {
             } 
         ];
         clusters.forEach(function (c) {
-            var url_prefix = 'http://' + c.server;
-            var sap_url = url_prefix + sap_link +'&cluster=' + c.cluster;
+            var url_prefix = 'http://' + c.server + '/data?' + 'cluster=' + c.cluster + '&';
+            var sap_url = url_prefix + sap_link;
             var cluster_title = '<a id="toggle-fullscreen" class="btn btn-block btn-primary" data-toggle="button" href="'
                                 + sap_url + '" target="_blank">' + c.name + '</a>';
             gallery.append(cluster_title);
             metrics.forEach(function (metric) {
-                var img_link = url_prefix + metric.fullscreen + '&cluster=' + c.cluster;
-                var img_src = url_prefix + metric.thumbnail + '&cluster=' + c.cluster;
-                var details_url = url_prefix + metric.details + '&cluster=' + c.cluster;
-                var related_url = url_prefix + metric.related + '&cluster=' + c.cluster;
+                var img_link = url_prefix + metric.fullscreen;
+                var img_src = url_prefix + metric.thumbnail;
+                var details_url = url_prefix + metric.details;
+                var related_url = url_prefix + metric.related;
                 var img_id = 'gallery-' + c.name + '-' + metric;
                 $('<a data-gallery="gallery"/>')
                     .append($('<img>').prop('src', img_src)
